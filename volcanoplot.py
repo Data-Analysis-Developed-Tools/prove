2a1fa09e8ea55f7bb91b6e10aa1e7242db45f378
Franco Rosso <169049117+FrancoR72@users.noreply.github.com>
2025-01-26 17:37:44 +0100

    Update volcanoplot.py

--- volcanoplot.py
+++ volcanoplot.py
@@ -3,7 +3,6 @@ import pandas as pd
 import plotly.express as px
 from scipy.stats import ttest_ind
 import numpy as np
-import base64
 from io import BytesIO
 
 # Funzione per caricare i dati
@@ -65,18 +64,17 @@ def main():
         dati = carica_dati(file)
         if dati is not None:
             dati_preparati = prepara_dati(dati, fold_change_threshold, p_value_threshold)
-            if dati_preparati is not None and not dati_preparati.empty:
+            if dati_preparati is not None:
                 fig = crea_volcano_plot(dati_preparati, show_labels)
                 if fig is not None:
                     st.plotly_chart(fig)
-                    st.dataframe(dati_preparati)  # Visualizzazione della tabella delle variabili filtrate
-                    # Permettere il download dei dati filtrati come Excel
+                    # Convert DataFrame to Excel in memory and provide download
                     output = BytesIO()
                     with pd.ExcelWriter(output, engine='xlsxwriter') as writer:
                         dati_preparati.to_excel(writer, sheet_name='Filtered Data', index=False)
                         writer.save()
                     output.seek(0)
-                    st.download_button(label="Download Excel", data=output, file_name="filtered_data.xlsx", mime="application/vnd.ms-excel")
+                    st.download_button(label="Download Excel", data=output.getvalue(), file_name="filtered_data.xlsx", mime="application/vnd.openxmlformats-officedocument.spreadsheetml.sheet")
 
 if __name__ == "__main__":
     main()
