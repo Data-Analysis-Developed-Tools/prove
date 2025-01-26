248809131653efe37d5bec02e8886df427e72683
Franco Rosso <169049117+FrancoR72@users.noreply.github.com>
2025-01-26 13:34:49 +0100

    Update volcanoplot.py

--- volcanoplot.py
+++ volcanoplot.py
@@ -14,7 +14,7 @@ def carica_dati(file):
     return dati
 
 # Preparazione dei dati per il volcano plot
-def prepara_dati(dati, fold_change_threshold, p_value):
+def prepara_dati(dati, fold_change_threshold, p_value_threshold):
     if dati is not None and isinstance(dati.columns, pd.MultiIndex):
         classi = dati.columns.get_level_values(1).unique()
         risultati = []
@@ -24,8 +24,7 @@ def prepara_dati(dati, fold_change_threshold, p_value):
                 media_diff = np.log2(np.mean(valori[0]) / np.mean(valori[1]))
                 t_stat, p_val = ttest_ind(valori[0], valori[1], equal_var=False)
                 p_val_log = -np.log10(p_val) if p_val > 0 else None
-                p_value_log_threshold = -np.log10(p_value) if p_value > 0 else None
-                if abs(media_diff) >= fold_change_threshold and p_val_log >= p_value_log_threshold:
+                if abs(media_diff) >= fold_change_threshold and p_val_log >= p_value_threshold:
                     risultati.append([var, media_diff, p_val_log])
         risultati_df = pd.DataFrame(risultati, columns=['Variabile', 'Log2 Fold Change', '-log10(p-value)'])
         return risultati_df
@@ -54,6 +53,7 @@ def main():
     with st.form(key='my_form'):
         fold_change_threshold = st.number_input('Inserisci il valore soglia per il -log2FoldChange', value=2.0)
         p_value = st.number_input('Inserisci il valore soglia per il p-value', value=0.05, format='%f')
+        p_value_threshold = -np.log10(p_value) if p_value > 0 else None  # Convertiamo il p-value in -log10(p-value)
         submit_button = st.form_submit_button(label='Applica Filtri')
 
     # Opzione per mostrare o nascondere le etichette delle variabili nel grafico
@@ -62,7 +62,7 @@ def main():
     if file is not None and submit_button:
         dati = carica_dati(file)
         if dati is not None:
-            dati_preparati = prepara_dati(dati, fold_change_threshold, p_value)
+            dati_preparati = prepara_dati(dati, fold_change_threshold, p_value_threshold)
             if dati_preparati is not None:
                 fig = crea_volcano_plot(dati_preparati, show_labels)
                 if fig is not None:
