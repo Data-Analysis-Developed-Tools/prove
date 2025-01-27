1890e9211db164915df3a43b0266accdbd0c70dc
Franco Rosso <169049117+FrancoR72@users.noreply.github.com>
2025-01-27 17:24:03 +0100

    Update volcanoplot.py

--- volcanoplot.py
+++ volcanoplot.py
@@ -21,11 +21,9 @@ def main():
         data = pd.read_excel(uploaded_file)
         st.write("Dati caricati con successo!")
 
-        # Setpoint e visualizzazione tramite form
         with st.form(key='my_form'):
             threshold_pvalue = st.number_input('Inserisci il valore soglia per p-value', min_value=0.0, max_value=1.0, value=0.05)
             threshold_fold_change = st.number_input('Inserisci il valore soglia per fold change', value=1.0)
-            show_labels = st.checkbox("Mostra etichette")
             submit_button = st.form_submit_button(label='Genera Volcano Plot')
 
         if submit_button:
@@ -33,18 +31,21 @@ def main():
             simulated_log2FoldChange = np.random.normal(0, 1, num_rows)
             simulated_pvalues = np.random.uniform(0, 0.05, num_rows)
 
-            # Creazione del volcano plot con Plotly
             data['Log2FoldChange'] = simulated_log2FoldChange
             data['-Log10(p-value)'] = -np.log10(simulated_pvalues)
-            fig = px.scatter(data, x='Log2FoldChange', y='-Log10(p-value)', 
-                             hover_data=[data.columns[0], 'p-value', 'Log2FoldChange'],
-                             size_max=10)
+
+            fig = px.scatter(
+                data, 
+                x='Log2FoldChange', 
+                y='-Log10(p-value)',
+                hover_data=[data.columns[0], ('p-value', np.power(10, -data['-Log10(p-value)'])), 'Log2FoldChange'],
+                size_max=10
+            )
             fig.update_traces(marker=dict(size=6))  # Ridurre la dimensione dei marker
             fig.add_vline(x=0, line_width=2, line_dash="dash", line_color="red")  # Linea verticale
             fig.update_layout(showlegend=False, hovermode='closest')
             st.plotly_chart(fig, use_container_width=True)
 
-            # Creazione del link di download per il nuovo file Excel
             output_df = data[['Etichette', 'Log2FoldChange', '-Log10(p-value)', 'p-value']]
             file_name = os.path.splitext(uploaded_file.name)[0] + "_values_from_volcano_plot.xlsx"
             link = create_download_link(output_df, file_name)
