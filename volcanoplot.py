0053f2c4b7a68994efbadf981c5034694064ed68
Franco Rosso <169049117+FrancoR72@users.noreply.github.com>
2025-01-28 13:39:42 +0100

    Update volcanoplot.py

--- volcanoplot.py
+++ volcanoplot.py
@@ -11,15 +11,15 @@ import plotly.graph_objects as go  # Import per le funzioni aggiuntive di Plotly
 def carica_dati(file):
     try:
         dati = pd.read_excel(file, header=[0, 1], index_col=0)
+        classi = dati.columns.get_level_values(1).unique()
+        return dati, classi
     except ValueError:
         st.error("Il file caricato non ha due livelli di intestazione come richiesto.")
-        return None
-    return dati
+        return None, None
 
 # Preparazione dei dati per il volcano plot
 def prepara_dati(dati, fold_change_threshold, p_value_threshold):
     if dati is not None and isinstance(dati.columns, pd.MultiIndex):
-        classi = dati.columns.get_level_values(1).unique()
         risultati = []
         for var in dati.index:
             valori = [dati.loc[var, dati.columns.get_level_values(1) == classe].dropna().values for classe in classi]
@@ -30,10 +30,10 @@ def prepara_dati(dati, fold_change_threshold, p_value_threshold):
                 if abs(media_diff) >= fold_change_threshold and p_val_log >= p_value_threshold:
                     risultati.append([var, media_diff, p_val_log])
         risultati_df = pd.DataFrame(risultati, columns=['Variabile', 'Log2 Fold Change', '-log10(p-value)'])
-        return risultati_df, classi
+        return risultati_df
     else:
         st.error("Il dataframe non contiene un indice multi-livello come atteso.")
-        return None, None
+        return None
 
 # Funzione per creare e salvare il grafico
 def crea_volcano_plot(dati, show_labels, classi):
@@ -86,24 +86,4 @@ def main():
 
     # Form per inserire il p-value e il fold change desiderati
     with st.form(key='my_form'):
-        fold_change_threshold = st.number_input('Inserisci il valore soglia per il Log2FoldChange', value=2.0)
-        p_value = st.number_input('Inserisci il valore soglia per il p-value', value=0.05, format='%f')
-        p_value_threshold = -np.log10(p_value) if p_value > 0 else None
-        submit_button = st.form_submit_button(label='Applica Filtri')
-
-    # Opzione per mostrare o nascondere le etichette delle variabili nel grafico
-    show_labels = st.checkbox("Mostra etichette delle variabili", value=True)
-
-    if file is not None and submit_button:
-        dati, classi = carica_dati(file)
-        if dati is not None:
-            dati_preparati, classi = prepara_dati(dati, fold_change_threshold, p_value_threshold)
-            if dati_preparati is not None:
-                fig = crea_volcano_plot(dati_preparati, show_labels, classi)
-                if fig is not None:
-                    st.plotly_chart(fig)
-    else:
-        st.info("Carica un file per procedere.")
-
-if __name__ == "__main__":
-    main()
+        fold_change_threshold = st.number_input('Inserisci il valore soglia per il Log2FoldChange', value=
