c02e27836de4a96947d41bd206d2cf9050de0533
Franco Rosso <169049117+FrancoR72@users.noreply.github.com>
2025-01-28 15:58:49 +0100

    Update volcanoplot.py

--- volcanoplot.py
+++ volcanoplot.py
@@ -1,8 +1,9 @@
 import streamlit as st
 import pandas as pd
+import numpy as np
+import matplotlib.colors as mcolors
 import plotly.express as px
 from scipy.stats import ttest_ind
-import numpy as np
 import plotly.graph_objects as go
 
 # Funzione per caricare i dati
@@ -31,30 +32,29 @@ def prepara_dati(dati, classi, fold_change_threshold, p_value_threshold):
                 media_diff = np.log2(np.mean(valori[0]) / np.mean(valori[1]))
                 t_stat, p_val = ttest_ind(valori[0], valori[1], equal_var=False)
                 p_val_log = -np.log10(p_val) if p_val > 0 else None
-                if abs(media_diff) >= fold_change_threshold and p_val_log >= p_value_threshold:
-                    risultati.append([var, media_diff, p_val_log, media_log[var]])
-        risultati_df = pd.DataFrame(risultati, columns=['Variabile', 'Log2FoldChange', '-log10(p-value)', 'MediaLog'])
+                log10_pval_log2fc = p_val_log * media_diff
+                risultati.append([var, media_diff, p_val_log, log10_pval_log2fc])
+        risultati_df = pd.DataFrame(risultati, columns=['Variabile', 'Log2FoldChange', '-log10(p-value)', '-log10(p-value x Log2FoldChange)'])
         return risultati_df
     else:
         st.error("Il dataframe non contiene un indice multi-livello come atteso.")
         return None
 
 # Crea il volcano plot con linee e annotazioni
-def crea_volcano_plot(dati, classi, show_labels, size_by_media, color_by_media):
+def crea_volcano_plot(dati, classi, show_labels):
     if dati is not None:
-        size = dati['MediaLog'] * 2 if size_by_media else None
-        color = dati['MediaLog'] if color_by_media else None
         fig = px.scatter(dati, x='Log2FoldChange', y='-log10(p-value)', text='Variabile' if show_labels else None,
-                         hover_data=['Variabile'], size=size, color=color,
-                         color_continuous_scale='RdYlBu_r',
-                         size_max=30)
-        fig.add_trace(go.Scatter(x=[0, 0], y=[0, dati['-log10(p-value)'].max()], mode='lines', line=dict(color='orange', width=2)))
-        fig.add_annotation(x=-2, y=dati['-log10(p-value)'].max()*1.05, text=f"Over-expression in {classi[1]}", showarrow=False, font=dict(color="red", size=16))
-        fig.add_annotation(x=2, y=dati['-log10(p-value)'].max()*1.05, text=f"Over-expression in {classi[0]}", showarrow=False, font=dict(color="green", size=16))
+                         hover_data=['Variabile'])
         fig.update_layout(title='Volcano Plot', xaxis_title='Log2FoldChange', yaxis_title='-log10(p-value)')
         return fig
     else:
         return None
+
+# Colora le celle della tabella
+def colora_celle(val):
+    norm = mcolors.TwoSlopeNorm(vmin=-1, vcenter=0, vmax=1)
+    rgba = mcolors.to_hex(mcolors.to_rgba(mcolors.TABLEAU_COLORS['tab:blue'], norm(val)))
+    return f'background-color: {rgba}'
 
 # Streamlit App
 def main():
@@ -65,9 +65,6 @@ def main():
     with st.form(key='my_form'):
         fold_change_threshold = st.number_input('Inserisci il valore soglia per il Log2FoldChange', value=0.0)
         p_value_threshold = st.number_input('Inserisci il valore soglia per il -log10(p-value)', value=0.05)
-        show_labels = st.checkbox("Mostra etichette delle variabili", value=True)
-        size_by_media = st.checkbox("Dimensiona punti per media valori assoluti inter-tesi", value=False)
-        color_by_media = st.checkbox("Colora punti per media dei valori assoluti inter-tesi", value=False)
         submit_button = st.form_submit_button(label='Applica Filtri')
 
     if file is not None and submit_button:
@@ -75,12 +72,11 @@ def main():
         if dati is not None:
             dati_preparati = prepara_dati(dati, classi, fold_change_threshold, p_value_threshold)
             if dati_preparati is not None:
-                fig = crea_volcano_plot(dati_preparati, classi, show_labels, size_by_media, color_by_media)
+                fig = crea_volcano_plot(dati_preparati, classi, True)
                 if fig is not None:
                     st.plotly_chart(fig)
-                    # Visualizza i dati sotto il grafico in forma di tabella
-                    st.write("Dati visibili attualmente nel grafico:")
-                    st.dataframe(dati_preparati)
+                    # Mostra la tabella con colorazione
+                    st.dataframe(dati_preparati.style.applymap(colora_celle, subset=['-log10(p-value x Log2FoldChange)']))
                 else:
                     st.error("Il grafico non contiene dati da visualizzare.")
             else:
